c322f595e6455cb19f39c51546b9a04c56871742
ArielHdezB <72314229+ArielHdezB@users.noreply.github.com>
2021-06-04 11:43:54 -0500

    Create practica_2.php

---
+++ practica_2.php
@@ -0,0 +1,13 @@
+<?php
+  $VAR1=0;
+  $VAR1=$_GET['sensor'];
+?>
+
+<html>
+  <head>
+    <title>Sensores PHP</title>
+  </head>
+  <body>
+    <H3>Sensor = <?php echo $VAR1 ?></H3>
+  </body>
+</html>
